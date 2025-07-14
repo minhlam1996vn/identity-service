99f27dfc84fe5311323525bc7dace53e4d2f971a
Minh Lam <minhlam1996vn@gmail.com>
2025-07-14 11:26:38 +0700

    crud basic

--- src/main/java/com/lamldm/identity_service/service/UserService.java
+++ src/main/java/com/lamldm/identity_service/service/UserService.java
@@ -1,10 +1,13 @@
 package com.lamldm.identity_service.service;
 
 import com.lamldm.identity_service.dto.request.UserCreationRequest;
+import com.lamldm.identity_service.dto.request.UserUpdateRequest;
 import com.lamldm.identity_service.entity.User;
 import com.lamldm.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +24,28 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public User updateUser(String userID, UserUpdateRequest request) {
+        User user = getUser(userID);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userID) {
+        userRepository.deleteById(userID);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
