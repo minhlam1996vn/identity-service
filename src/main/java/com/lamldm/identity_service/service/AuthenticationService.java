e28a7d2f1ac4ed500c3ab9b14521e0bc00c85cfd
Minh Lam <minhlam1996vn@gmail.com>
2025-07-28 09:04:01 +0700

    generateToken with JWT

--- src/main/java/com/lamldm/identity_service/service/AuthenticationService.java
+++ src/main/java/com/lamldm/identity_service/service/AuthenticationService.java
@@ -1,15 +1,26 @@
 package com.lamldm.identity_service.service;
 
 import com.lamldm.identity_service.dto.request.AuthenticationRequest;
+import com.lamldm.identity_service.dto.response.AuthenticationResponse;
 import com.lamldm.identity_service.exception.AppException;
 import com.lamldm.identity_service.exception.ErrorCode;
 import com.lamldm.identity_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +28,43 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest authenticationRequest) {
+    @NonFinal // Đánh dấu để không thêm vào constructor
+    protected static final String SIGNER_KEY = "30cE2KCuTKlzWu5bGPGb3CNNZOINTGQAdfXECdHzAoe0SMVgyAFK55j+/rFOigS0";
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         var user = userRepository.findByUsername(authenticationRequest.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(authenticationRequest.getPassword(), user.getPassword());
+        boolean authenticated = passwordEncoder.matches(authenticationRequest.getPassword(), user.getPassword());
+
+        if (!authenticated) throw new AppException(ErrorCode.UNAUTHENTICATED);
+
+        var token = generateToken(user.getUsername());
+
+        return AuthenticationResponse.builder().token(token).authenticated(true).build();
+    }
+
+    private String generateToken(String username) { // https://generate-random.org/encryption-key-generator
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("mldev.com") // domain
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
+                .claim("customClaim", "Custom")
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Cannot create token", e);
+            throw new RuntimeException(e);
+        }
     }
 }
