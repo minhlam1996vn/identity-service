ab80204160d2ef5cdb693b7755f90e3ba249b847
Minh Lam <minhlam1996vn@gmail.com>
2025-07-31 15:25:46 +0700

    Authorize

--- src/main/java/com/lamldm/identity_service/controller/UserController.java
+++ src/main/java/com/lamldm/identity_service/controller/UserController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
 @RequestMapping("/users")
 @RequiredArgsConstructor()
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserController {
     UserService userService;
 
@@ -29,6 +32,14 @@ public class UserController {
 
     @GetMapping
     ApiResponse<List<UserResponse>> getUsers() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("username: {}", authentication.getName());
+        authentication.getAuthorities().forEach(
+                grantedAuthority -> log.info("authority: {}", grantedAuthority.getAuthority()
+                )
+        );
+
         return ApiResponse.<List<UserResponse>>builder()
                 .result(userService.getUsers())
                 .build();
--- src/main/java/com/lamldm/identity_service/service/AuthenticationService.java
+++ src/main/java/com/lamldm/identity_service/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import com.lamldm.identity_service.dto.request.AuthenticationRequest;
 import com.lamldm.identity_service.dto.request.IntrospectRequest;
 import com.lamldm.identity_service.dto.response.AuthenticationResponse;
 import com.lamldm.identity_service.dto.response.IntrospectResponse;
+import com.lamldm.identity_service.entity.User;
 import com.lamldm.identity_service.exception.AppException;
 import com.lamldm.identity_service.exception.ErrorCode;
 import com.lamldm.identity_service.repository.UserRepository;
@@ -20,11 +21,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.StringJoiner;
 
 import static org.hibernate.query.sqm.tree.SqmNode.log;
 
@@ -63,20 +66,20 @@ public class AuthenticationService {
 
         if (!authenticated) throw new AppException(ErrorCode.UNAUTHENTICATED);
 
-        var token = generateToken(user.getUsername());
+        var token = generateToken(user);
 
         return AuthenticationResponse.builder().token(token).authenticated(true).build();
     }
 
-    private String generateToken(String username) { // https://generate-random.org/encryption-key-generator
+    private String generateToken(User user) { // https://generate-random.org/encryption-key-generator
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                .subject(username)
+                .subject(user.getUsername())
                 .issuer("mldev.com") // domain
                 .issueTime(new Date())
                 .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
-                .claim("userId", "Custom")
+                .claim("scope", buildScope(user))
                 .build();
 
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
@@ -90,5 +93,13 @@ public class AuthenticationService {
             log.error("Cannot create token", e);
             throw new RuntimeException(e);
         }
+    }
+
+    private String buildScope(User user) {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles()))
+            user.getRoles().forEach(stringJoiner::add);
+
+        return stringJoiner.toString();
     }
 }
