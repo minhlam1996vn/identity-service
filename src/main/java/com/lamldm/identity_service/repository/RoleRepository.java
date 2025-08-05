f32821ba6b65688e713ce6863270115bd7b33a8f
Minh Lam <minhlam1996vn@gmail.com>
2025-08-05 16:42:00 +0700

    Role & Permissions

---
+++ src/main/java/com/lamldm/identity_service/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.lamldm.identity_service.repository;
+
+import com.lamldm.identity_service.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
---
+++ src/main/java/com/lamldm/identity_service/service/RoleService.java
@@ -0,0 +1,47 @@
+package com.lamldm.identity_service.service;
+
+import com.lamldm.identity_service.dto.request.RoleRequest;
+import com.lamldm.identity_service.dto.response.RoleResponse;
+import com.lamldm.identity_service.mapper.RoleMapper;
+import com.lamldm.identity_service.repository.PermissionRepository;
+import com.lamldm.identity_service.repository.RoleRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class RoleService {
+    PermissionRepository permissionRepository;
+    RoleRepository roleRepository;
+
+    RoleMapper roleMapper;
+
+    public RoleResponse create(RoleRequest request) {
+        var role = roleMapper.toRole(request);
+        var permissions = permissionRepository.findAllById(request.getPermissions());
+
+        role.setPermissions(new HashSet<>(permissions));
+        role = roleRepository.save(role);
+
+        return roleMapper.toRoleResponse(role);
+    }
+
+    public List<RoleResponse> getAll() {
+        return roleRepository.findAll()
+                .stream()
+                .map(roleMapper::toRoleResponse)
+                .toList();
+    }
+
+    public void delete(String role) {
+        roleRepository.deleteById(role);
+    }
+}
