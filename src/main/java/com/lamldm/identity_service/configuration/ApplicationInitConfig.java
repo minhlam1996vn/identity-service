471dd10f5b3f870f43af794eef6decd0d4c8ad31
Minh Lam <minhlam1996vn@gmail.com>
2025-08-21 15:25:21 +0700

    Unit test with h2 database

--- src/main/java/com/lamldm/identity_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/lamldm/identity_service/configuration/ApplicationInitConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,12 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty
+            (
+                    prefix = "spring",
+                    value = "datasource.driverClassName",
+                    havingValue = "com.mysql.jdbc.Driver"
+            )
     ApplicationRunner applicationRunner(UserRepository userRepository) { // Sẽ được khởi chạy mỗi khi Application được start
         return args -> {
             if (userRepository.findByUsername("admin").isEmpty()) {
