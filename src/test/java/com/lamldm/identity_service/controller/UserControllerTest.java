fff64d093b551a0dd1c1556ffc2339cabb56264e
Minh Lam <minhlam1996vn@gmail.com>
2025-08-21 14:06:09 +0700

    Unit test [Test controller]

---
+++ src/test/java/com/lamldm/identity_service/controller/UserControllerTest.java
@@ -0,0 +1,108 @@
+package com.lamldm.identity_service.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.lamldm.identity_service.dto.request.UserCreationRequest;
+import com.lamldm.identity_service.dto.response.UserResponse;
+import com.lamldm.identity_service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+    private UserCreationRequest userCreationRequest;
+    private UserResponse userResponse;
+
+    @BeforeEach
+    void initData() {
+        LocalDate dob = LocalDate.of(1990, 1, 1);
+
+        userCreationRequest = UserCreationRequest.builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("6b92af46")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+    }
+
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(userCreationRequest);
+
+        Mockito.when(userService.createUser(userCreationRequest))
+                .thenReturn(userResponse); // Sẽ bỏ qua service nữa mà trả về userResponse
+
+        // WHEN, THEN
+        mockMvc
+                // WHEN
+                .perform
+                        (MockMvcRequestBuilders
+                                .post("/users")
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                .content(content)
+                        )
+                // THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id")
+                        .value("6b92af46"));
+    }
+
+    @Test
+    void createUser_usernameInvalid_fail() throws Exception {
+        // GIVEN
+        userCreationRequest.setUsername("jo");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(userCreationRequest);
+
+        // WHEN, THEN
+        mockMvc
+                // WHEN
+                .perform
+                        (MockMvcRequestBuilders
+                                .post("/users")
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                .content(content)
+                        )
+                // THEN
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1003))
+                .andExpect(MockMvcResultMatchers.jsonPath("message")
+                        .value("Username must be at least 3 characters"));
+    }
+}
